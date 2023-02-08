9918df66d560dbb5d6c8ccd75e39f179be20633b
chtof <chtof@itu.dk>
2023-02-08 21:33:17 +0100

    start test refactoring

---
+++ MinitwitTests/MinitwitTests.cs
@@ -0,0 +1,117 @@
+
+using System.Data;
+using System.Data.Common;
+using System;
+using System.Data.Sqlite;
+using Xunit; // unsure if this is needed
+//using Minitwit;
+namespace MinitwitTests;
+
+
+
+public class DatabaseFixture : IDisposable
+{
+
+    public DatabaseFixture()
+    {
+        // Setup
+        Db = new SQLiteConnection("Data Source = database.db;Version=3;New=True;Compress=True;");
+    }
+
+    public void Dispose()
+    {
+        // Teardown
+        Db.Dispose();
+    }
+
+    public SQLiteConnection Db { get; private set; }
+}
+
+public class MinitwitTests : IClassFixture<DatabaseFixture>
+{
+    DatabaseFixture _fixture;
+
+    public MinitwitTests(DatabaseFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+
+
+    [Theory]
+    [InlineData("user1", "default", "default", "email@example.com")]
+    [InlineData("user1", "default", "default", "email@example.com")]
+    [InlineData("", "default", "default", "email@example.com")]
+    [InlineData("meh", "", "default", "email@example.com")]
+    [InlineData("meh", "x", "y", "email@example.com")]
+    [InlineData("meh", "foo", "foo", "broken")]
+    public void TestRegister(String username, String password, String password2, String email)
+    {
+        // Arrange
+        var db = _fixture.Db;
+
+        // Act
+        var result = Minitwit.Register(username, password, password2, email);
+
+        // Assert
+        Assert.Equal("You were successfully registered and can login now", result);
+        Assert.Equal("The username is already taken", result);
+        Assert.Equal("You have to enter a username", result);
+        Assert.Equal("You have to enter a password", result);
+        Assert.Equal("The two passwords do not match", result);
+        Assert.Equal("You have to enter a valid email address", result);
+    }
+
+    [Theory]
+    [InlineData("user1", "default")]
+    [InlineData("user1", "wrongpassword")]
+    [InlineData("user2", "wrongpassword")]
+    public void TestLogin(String username, String password)
+    {
+        // Arrange
+        var db = _fixture.Db;
+        Minitwit.Register("user1", "default", "default", "email@example.com");
+
+        // Act
+        var result = Minitwit.Login(username, password);
+
+        // Assert
+        Assert.Equal("You were logged in", result);
+        Assert.Equal("Invalid password", result);
+        Assert.Equal("Invalid username", result);
+    }
+
+    [Fact]
+    public void TestLogout()
+    {
+        // Arrange
+        var db = _fixture.Db;
+        Minitwit.Register("user1", "default", "default", "email@example.com");
+
+        // Act
+        var result = Minitwit.Logout();
+
+        // Assert
+        Assert.Equal("You were logged out", result);
+    }
+
+    [Theory]
+    [InlineData("test message 1")]
+    [InlineData("<test message 2>")]
+    public void TestAddMessage(String message)
+    {
+        // Arrange
+        var db = _fixture.Db;
+        Minitwit.Register("user1", "default", "default", "email@example.com");
+        Minitwit.Login("user1", "default");
+
+        // Act
+        Minitwit.AddMessage(message);
+        result = Minitwit.get("/"); // pretty much stolen from python tests, most likely not correct
+
+        // Assert
+        Assert.Equal("test message 1", result);
+    }
+
+
+}
