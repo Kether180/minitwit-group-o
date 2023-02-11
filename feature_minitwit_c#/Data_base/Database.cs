85f21972851df90093dde2d858ae0eecc6e89940
Alexander <alexander180@pop-os.localdomain>
2023-02-11 13:05:52 +0100

    methods-getuserid-formatdatetime

--- feature_minitwit_c#/Data_base/Database.cs
+++ feature_minitwit_c#/Data_base/Database.cs
@@ -2,6 +2,7 @@ using System.Data;
 using System.Data.Common;
 using System;
 using System.Data.SQLite;
+using System.Data.SqlClient;
 
 namespace Database;
 
@@ -59,6 +60,8 @@ public class Database
     }
 
     public List<Dictionary<string, object>> QueryDb(string query, List<SQLiteParameter> parameters)
+
+    // Queries the database and returns a list of dictionaries
     {
         List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
 
@@ -87,5 +90,69 @@ public class Database
     }
 
 
+    public int GetUserId(string username) // convenience method to look up the id for a username.
+    {
+        int userId = 0;
+
+        using (SQLiteCommand cmd = new SQLiteCommand("select user_id from user where username = @username", Db))
+        {
+            cmd.Parameters.AddWithValue("@username", username);
+
+            using (SQLiteDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    userId = reader.GetInt32(0);
+                }
+            }
+        }
+
+        return userId;
+    }
+
+
+    public string FormatDatetime(int timestamp) // Format a timestamp for display.
+    {
+        return DateTime.FromFileTimeUtc(timestamp).ToString("yyyy-MM-dd @ HH:mm");
+    }
 
 }
+
+
+// no clue if we are using the md5 library -- ? 
+/*
+    public static string GravatarUrl(string email, int size = 80)
+    {
+        using (var md5 = System.Security.Cryptography.MD5.Create())
+        {
+            var emailBytes = System.Text.Encoding.UTF8.GetBytes(email.Trim().ToLower());
+            var hashBytes = md5.ComputeHash(emailBytes);
+            var hash = BitConverter.ToString(hashBytes).Replace("-", string.Empty).ToLower();
+            return $"http://www.gravatar.com/avatar/{hash}?d=identicon&s={size}";
+        }
+    }
+}
+
+*/
+/*
+
+    public string GravatarUrl(string email, int size = 80)
+    {
+        using (var md5 = MD5.Create())
+        {
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(email.Trim().ToLower());
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            // Convert the byte array to hexadecimal string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2"));
+            }
+
+            return $"http://www.gravatar.com/avatar/{sb.ToString().ToLower()}?d=identicon&s={size}";
+        }
+    }
+}
+
+*/
