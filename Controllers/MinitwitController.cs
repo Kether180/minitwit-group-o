2102c4ea04b70c6271c070a3ec65510eb0266851
Mikkel Munkholm Blak Nilsson <muni@itu.dk>
2023-02-18 11:16:20 +0100

    initial commit

---
+++ Controllers/MinitwitController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Minitwit7.data;
+using Minitwit7.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Minitwit7.Controllers
+{
+    [Route("api/[controller]")]
+    public class MinitwitController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public MinitwitController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("/AddUser")]
+        public async Task<ActionResult<List<User>>> AddUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(_context.Users.ToList());
+        }
+    }
+}
+
---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Minitwit7.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string PwHash { get; set; } = "";
+    }
+}
+
