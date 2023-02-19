40eaf7d5555ee6f994881a7d75155ff205a4239f
nlog14 <ngedeon14@yahoo.com>
2023-02-19 17:22:53 +0100

    get messages

--- Controllers/MinitwitController.cs
+++ Controllers/MinitwitController.cs
@@ -29,6 +29,25 @@ namespace Minitwit7.Controllers
 
             return Ok(_context.Users.ToList());
         }
+
+        [HttpPost]
+        [Route("/AddMsg")]
+        public async Task<ActionResult<List<Message>>> AddUMsg(Message msg)
+        {
+            _context.Messages.Add(msg);
+            await _context.SaveChangesAsync();
+            return Ok(_context.Messages.ToList());
+        }
+
+        [HttpGet]
+        [Route("/GetMsgs")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult<List<Message>>> GetMsgs()
+        {
+            var msgs = _context.Messages.ToList();
+            if(msgs == null){return NoContent();}
+            else{return Ok(msgs);}
+        }
     }
 }
-
