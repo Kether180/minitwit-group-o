3d2040171eab6608f911570b50bfe3d7d9cf265f
Hristiyana Toteva <hritote@dfds.com>
2023-03-07 14:52:50 +0100

    frontend folder has been added

---
+++ minitwit-frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Registration from './components/Registration';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Registration /><br>
+      </br>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
