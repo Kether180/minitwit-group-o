b423f51e5203f69c850e6c487328666afe73a305
Hristiyana Toteva <hristiqnatoteva9718@gmail.com>
2023-04-25 09:52:19 +0200

    follow button added

--- minitwit-frontend/src/components/Timeline.js
+++ minitwit-frontend/src/components/Timeline.js
@@ -25,18 +25,27 @@ function Timeline(){
       <br/>
 
       <br/>      
-      <div className= 'container'>
-        <div className='centered'>
-          
-          <h1>Timeline</h1>
-          {msgs.map((item) => (
-            <div key={item.pub_date}>
-              <p>{item.user}</p>
-              <p>{item.content}</p>
+      <div className="container">
+      <div className="centered">
+        <h1 className="timeline-header">Timeline</h1>
+        {msgs.map((item) => (
+          <div key={item.pub_date} className="post-container">
+            <div className="post-header">
+              <p className="post-user">{item.user}</p>
+              <div className="post-buttons">
+                <button className="follow-button">Follow</button>
+              </div>
             </div>
-          ))}
-        </div>
+            <div className="post-content">
+              <div className="post-box">
+                <p className="post-text">{item.content}</p>
+              </div>
+              <br/>
+            </div>
+          </div>
+        ))}
       </div>
+    </div>
       </>
     );
 }
